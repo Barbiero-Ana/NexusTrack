9d1746c78f0a38aa5714617a26ed40713dfdb066
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-05-13 10:48:30 -0400

    pra cima

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// Script para abrir e fechar o menu hamburguer
-document.getElementById('hamburger').addEventListener('click', function() {
-  const menu = document.querySelector('.menu');
+// script.js
+
+// Selecionando o ícone do hambúrguer e o menu
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+// Adicionando evento de clique no hambúrguer para alternar a classe "active" no menu
+hamburger.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
